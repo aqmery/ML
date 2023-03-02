fdd94ce4fbfd383280fcf68d31abeb7423d5da46
Henry De Lange <henrydelange8412@gmail.com>
2023-03-03 00:30:05 +0100

    cleaned up the main file, re-organized functions, added back_propagation and total_loss functions

--- perceptron_learning_rule/main.py
+++ perceptron_learning_rule/main.py
@@ -3,6 +3,7 @@ import itertools
 from scipy.special import expit
 random.seed(1782152)
 
+
 class Perceptron:
     """
     creates a Perceptron object.
@@ -14,7 +15,7 @@ class Perceptron:
         self.weights = weights
         self.bias = bias
         self.name = name
-        self.eta = 0.1
+        self.eta = .1
 
     def __str__(self):
         return f"weights = {self.weights}, bias = {self.bias}, name = {self.name}"
@@ -35,51 +36,45 @@ class Perceptron:
         :return: returns a step_function call with weighted_sum as argument.
         """
         weighted_sum = 0
-        for i in range(len(inputs)):
-            weighted_sum += inputs[i] * self.weights[i]
+        for i_in in range(len(inputs)):
+            weighted_sum += inputs[i_in] * self.weights[i_in]
         weighted_sum = weighted_sum + self.bias
         return self.step_function(weighted_sum)
 
-    def update(self, x, y):
-        loss_weight = []
-        error = None
-        for i in range(len(x)):
-            # print(x[i])
-            # print(self.activate(x[i]))
-            # print(self.weights, self.bias)
-            # print(y)
-            # print(x[i][0])
-            t = round(expit(self.weights[0]*x[i][0] + self.weights[1]*x[i][1] + self.bias), 3)
-            loss_weight.append(t)
-            # print(t)
-            error = round(t*((1-t)*-(y[i]-t)), 3)
-            # print(error)
-            self.back_propagation(x[i], y[i], error)
-        #     print("")
-        # print(loss_weight)
+    def update(self, p_input, activation):
+        weight_error = []
+        for i_input in range(len(p_input)):
+            temp_error = round(expit(self.weights[0]*p_input[i_input][0] +
+                                     self.weights[1]*p_input[i_input][1] + self.bias), 3)
+            weight_error.append(temp_error)
+        if self.total_loss(weight_error, activation) < .05:
+            return True
+        self.back_propagation(weight_error, activation)
+        return False
 
-    def back_propagation(self, inputs, activation, error):
-        # print("in back propagation")
-        # print(inputs)
-        # print(error)
-        print(self.weights)
-        for i in range(len(self.weights)):
-            self.weights[i] = self.weights[i]-self.eta*activation*error
-        self.bias = self.bias-self.eta*1*error
+    def back_propagation(self, weight_error, activation):
+        for i_result in range(len(activation)):
+            error = round(weight_error[i_result]*((1-weight_error[i_result])*
+                                                  -(activation[i_result]-weight_error[i_result])), 3)
+            for i_weight in range(len(self.weights)):
+                self.weights[i_weight] = self.weights[i_weight] - self.eta * activation[i_result] * error
+            self.bias = self.bias - self.eta * 1 * error
 
-
-
+    def total_loss(self, weight_error, activation):
+        error_sum = 0
+        for i_error in range(len(weight_error)):
+            error_sum += (activation[i_error]-weight_error[i_error])**2
+        loss = error_sum/(2*(len(weight_error)))
+        return loss
 
 
 x = list(itertools.product([0, 1], repeat=2))
 y = [0, 0, 0, 1]
 
-
 p_and = Perceptron([-0.5, 0.5], 1.5, "AND")
-
+print(p_and)
 
 inputs = 2
-# test perceptrons with 2 different inputs
 p_lst_2 = [p_and]
 for p in p_lst_2:
     print(p.name)
@@ -90,15 +85,17 @@ for p in p_lst_2:
     print("")
 
 
-for i in range(140):
-    p_and.update(x, y)
-    print(p_and)
+stop = False
+count = 0
+while not stop:
+    count += 1
+    if p_and.update(x, y):
+        stop = True
+    if count == 1000:
+        stop = True
 
-# p_and.update(x, y)
 
-
-
-# test perceptrons with 2 different inputs
+print(p_and)
 p_lst_2 = [p_and]
 for p in p_lst_2:
     print(p.name)
@@ -107,59 +104,3 @@ for p in p_lst_2:
         for j in range(inputs):
             print(i, "  ", j, "  |", p.activate([i, j]))
     print("")
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def update(self, activation):
-#     print(expit(self.weights[0]*activation[0]+self.weights[1]*activation[1]+self.bias))
-#     self.loss_weight.append(expit(self.weights[0]*activation[0]+self.weights[1]*activation[1]+self.bias))
-#
-#     # for i in range(len(self.weights)):
-#     #     print(activation[i]*self.weights[i]+self.bias)
-#     #     print(expit(activation[i]*self.weights[i]+self.bias))
-#
-#
-# def loss(self, outputs):
-#     loss = []
-#     for j in range(len(self.loss_weight)):
-#         loss.append((outputs[j]-self.loss_weight[j])**2)
-#     print(sum(loss)/(2*len(outputs)))
-
-
-# x = list(itertools.product([0, 1], repeat=2))
-# y = [0, 0, 0, 1]
-#
-# # p_and = Perceptron([random.uniform(-1, 1) for _ in range(2)],
-# #                    random.uniform(-1, 1),
-# #                    "AND")
-#
-# p_and = Perceptron([-0.5, 0.5], 1.5, "AND")
-#
-# for i in range(len(x)):
-#     print(p_and)
-#     print(x[i])
-#     p_and.update(x[i])
-#     if p_and.activate(x[i]) != y[i]:
-#         print("false")
-#     else:
-#         print("true")
-#     print("")
-#     print(p_and.activate(x[i]))
-#     print(y[i])
-#     print("---------")
-#
-# print(p_and.loss_weight)
-# p_and.loss(y)
-# # test_p_AND = Perceptron([0.5, 0.5], -1.0, "AND")
-
-
