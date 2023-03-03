33e303247fdfb6beb391faa55ee287cfe602101a
Henry De Lange <henrydelange8412@gmail.com>
2023-03-03 01:02:43 +0100

    moved Perceptron class to its own file, added a test_perceptron file

--- perceptron_learning_rule/main.py
+++ perceptron_learning_rule/main.py
@@ -1,106 +1,56 @@
 import random
 import itertools
-from scipy.special import expit
+from perceptron import Perceptron
 random.seed(1782152)
 
 
-class Perceptron:
-    """
-    creates a Perceptron object.
-    :param weights: takes in a list of floats as weights for the perceptron.
-    :param bias: takes in a float as bias for the perceptron.
-    :param name: takes in a string for the name of the perceptron.
-    """
-    def __init__(self, weights, bias, name):
-        self.weights = weights
-        self.bias = bias
-        self.name = name
-        self.eta = .1
-
-    def __str__(self):
-        return f"weights = {self.weights}, bias = {self.bias}, name = {self.name}"
-
-    def step_function(self, weighted_sum):
-        """
-        :param weighted_sum: gets a float and checks if it's higher than 0.
-        :return: returns 1 or 0 based on the weighted_sum.
-        """
-        if weighted_sum >= 0:
-            return 1
-        else:
-            return 0
-
-    def activate(self, inputs):
-        """
-        :param inputs: gets a list of ints and calculates the weighted sum based on the weights and the bias.
-        :return: returns a step_function call with weighted_sum as argument.
-        """
-        weighted_sum = 0
-        for i_in in range(len(inputs)):
-            weighted_sum += inputs[i_in] * self.weights[i_in]
-        weighted_sum = weighted_sum + self.bias
-        return self.step_function(weighted_sum)
-
-    def update(self, p_input, activation):
-        weight_error = []
-        for i_input in range(len(p_input)):
-            temp_error = round(expit(self.weights[0]*p_input[i_input][0] +
-                                     self.weights[1]*p_input[i_input][1] + self.bias), 3)
-            weight_error.append(temp_error)
-        if self.loss(weight_error, activation) < .05:
-            return True
-        self.back_propagation(weight_error, activation)
-        return False
-
-    def back_propagation(self, weight_error, activation):
-        for i_result in range(len(activation)):
-            error = round(weight_error[i_result]*((1-weight_error[i_result])*
-                                                  -(activation[i_result]-weight_error[i_result])), 3)
-            for i_weight in range(len(self.weights)):
-                self.weights[i_weight] = self.weights[i_weight] - self.eta * activation[i_result] * error
-            self.bias = self.bias - self.eta * 1 * error
-
-    def loss(self, weight_error, activation):
-        error_sum = 0
-        for i_error in range(len(weight_error)):
-            error_sum += (activation[i_error]-weight_error[i_error])**2
-        total_loss = error_sum/(2*(len(weight_error)))
-        return total_loss
+def print_perceptron(perceptron):
+    print(perceptron)
+    inputs = 2
+    p_lst = [perceptron]
+    for p in p_lst:
+        print(p.name)
+        print("in1, in2 | out")
+        for i in range(inputs):
+            for j in range(inputs):
+                print(i, "  ", j, "  |", p.activate([i, j]))
+        print("")
 
 
 x = list(itertools.product([0, 1], repeat=2))
-y = [0, 0, 0, 1]
-
+y_and = [0, 0, 0, 1]
 p_and = Perceptron([-0.5, 0.5], 1.5, "AND")
-print(p_and)
 
-inputs = 2
-p_lst_2 = [p_and]
-for p in p_lst_2:
-    print(p.name)
-    print("in1, in2 | out")
-    for i in range(inputs):
-        for j in range(inputs):
-            print(i, "  ", j, "  |", p.activate([i, j]))
-    print("")
-
+print_perceptron(p_and)
 
 stop = False
 count = 0
 while not stop:
     count += 1
-    if p_and.update(x, y):
+    if p_and.update(x, y_and):
         stop = True
     if count == 1000:
         stop = True
+print("amount of updates:", count)
+print("")
+
+print_perceptron(p_and)
 
 
-print(p_and)
-p_lst_2 = [p_and]
-for p in p_lst_2:
-    print(p.name)
-    print("in1, in2 | out")
-    for i in range(inputs):
-        for j in range(inputs):
-            print(i, "  ", j, "  |", p.activate([i, j]))
-    print("")
+y_xor = [0, 1, 1, 0]
+p_xor = Perceptron([-0.5, 0.5], 1.5, "XOR")
+
+print_perceptron(p_xor)
+
+stop = False
+count = 0
+while not stop:
+    count += 1
+    if p_xor.update(x, y_xor):
+        stop = True
+    if count == 1000:
+        stop = True
+print("amount of updates:", count)
+print("")
+
+print_perceptron(p_xor)
---
+++ perceptron_learning_rule/perceptron.py
@@ -0,0 +1,64 @@
+from scipy.special import expit
+
+class Perceptron:
+    """
+    creates a Perceptron object.
+    :param weights: takes in a list of floats as weights for the perceptron.
+    :param bias: takes in a float as bias for the perceptron.
+    :param name: takes in a string for the name of the perceptron.
+    """
+    def __init__(self, weights, bias, name):
+        self.weights = weights
+        self.bias = bias
+        self.name = name
+        self.eta = .1
+
+    def __str__(self):
+        return f"weights = {self.weights}, bias = {self.bias}, name = {self.name}"
+
+    def step_function(self, weighted_sum):
+        """
+        :param weighted_sum: gets a float and checks if it's higher than 0.
+        :return: returns 1 or 0 based on the weighted_sum.
+        """
+        if weighted_sum >= 0:
+            return 1
+        else:
+            return 0
+
+    def activate(self, inputs):
+        """
+        :param inputs: gets a list of ints and calculates the weighted sum based on the weights and the bias.
+        :return: returns a step_function call with weighted_sum as argument.
+        """
+        weighted_sum = 0
+        for i_in in range(len(inputs)):
+            weighted_sum += inputs[i_in] * self.weights[i_in]
+        weighted_sum = weighted_sum + self.bias
+        return self.step_function(weighted_sum)
+
+    def update(self, p_input, activation):
+        weight_error = []
+        for i_input in range(len(p_input)):
+            temp_error = round(expit(self.weights[0]*p_input[i_input][0] +
+                                     self.weights[1]*p_input[i_input][1] + self.bias), 3)
+            weight_error.append(temp_error)
+        if self.loss(weight_error, activation) < .05:
+            return True
+        self.back_propagation(weight_error, activation)
+        return False
+
+    def back_propagation(self, weight_error, activation):
+        for i_result in range(len(activation)):
+            error = round(weight_error[i_result]*((1-weight_error[i_result])*
+                                                  -(activation[i_result]-weight_error[i_result])), 3)
+            for i_weight in range(len(self.weights)):
+                self.weights[i_weight] = self.weights[i_weight] - self.eta * activation[i_result] * error
+            self.bias = self.bias - self.eta * 1 * error
+
+    def loss(self, weight_error, activation):
+        error_sum = 0
+        for i_error in range(len(weight_error)):
+            error_sum += (activation[i_error]-weight_error[i_error])**2
+        mean_squared_error = error_sum/(2*(len(weight_error)))
+        return mean_squared_error
