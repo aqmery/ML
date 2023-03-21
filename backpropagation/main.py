68243f663b524e01de268c88b63fdf48598ed5a5
Henry De Lange <henrydelange8412@gmail.com>
2023-03-21 02:01:55 +0100

    started working on the digits dataset

--- backpropagation/main.py
+++ backpropagation/main.py
@@ -2,14 +2,17 @@ import pandas as pd
 import numpy as np
 import random
 import itertools
+
 from sklearn.datasets import load_iris
+from sklearn.datasets import load_digits
+
 from neuron import Neuron
 from neuron_layer import NeuronLayer
 from neuron_network import NeuronNetwork
 random.seed(1782152)
 
 
-def print_neuron(network):
+def print_network(network):
     num_weights = len(network.neuron_layers[0].neurons[0].weights)
     print(f"{network}\n{network.name}")
     for w in range(num_weights):
@@ -22,6 +25,14 @@ def print_neuron(network):
 
 
 def create_network(layers, neurons_list, amount_weights, network_name):
+    """
+    creates and sets up a neural network with specified layers, neurons and weights.
+    :param layers: the amount of layers the network needs
+    :param neurons_list: the amount of neurons in each layer, given as a list
+    :param amount_weights: the amount of weights each neuron has in each layer, given as a list
+    :param network_name: the name of the network
+    :return: creates and returns a neural network
+    """
     neuron_layers = []
     for l in range(layers):
         neurons = []
@@ -32,63 +43,84 @@ def create_network(layers, neurons_list, amount_weights, network_name):
     return NeuronNetwork(neuron_layers, str(network_name))
 
 
-and_network = create_network(1, [1], [2], "and_network")
-inputs_and = list(itertools.product([0, 1], repeat=2))
-target_and = [0, 0, 0, 1]
+# and_network = create_network(1, [1], [2], "and_network")
+# inputs_and = list(itertools.product([0, 1], repeat=2))
+# target_and = [0, 0, 0, 1]
+#
+# print_network(and_network)
+#
+# and_network.train(inputs_and, target_and, 0.001, 10000, 0.5)
+#
+# print("")
+# print_network(and_network)
+# print("------------------------------------------------------------------------")
+#
+#
+# half_adder = create_network(2, [3, 2], [2, 3], "half_adder")
+# target_half_adder = [[0,0], [0,1], [0,1], [1,0]]
+# inputs_half_adder = list(itertools.product([0, 1], repeat=2))
+#
+# print_network(half_adder)
+#
+# half_adder.train(inputs_half_adder, target_half_adder, 0.01, 10000, 0.2)
+#
+# print("")
+# print_network(half_adder)
+# print("------------------------------------------------------------------------")
+#
+#
+# xor_network = create_network(2, [2, 1], [2, 2], "xor_network")
+# inputs_xor = list(itertools.product([0, 1], repeat=2))
+# target_xor = [0, 1, 1, 0]
+#
+# print_network(xor_network)
+#
+# xor_network.train(inputs_xor, target_xor, 0.001, 10000, 0.5)
+#
+# print("")
+# print_network(xor_network)
+# print("------------------------------------------------------------------------")
+#
+#
+# iris = load_iris()
+# df_iris = pd.DataFrame(data=np.c_[iris["data"], iris["target"]],
+#                      columns= iris["feature_names"] + ["target"])
+#
+# iris_network = create_network(2, [4, 3], [4, 4], "iris_network")
+# target_iris = []
+# for i in range(len(iris["target"])):
+#     if iris["target"][i] == 0.0:
+#         target_iris.append([1, 0, 0])
+#     elif iris["target"][i] == 1:
+#         target_iris.append([0, 1, 0])
+#     else:
+#         target_iris.append([0, 0, 1])
+# inputs_iris = [list(row[:4]) for row in df_iris.values]
+#
+# iris_network.train(inputs_iris, target_iris, 0.001, 1000, 0.5)
+#
+# results = [iris_network.collapse_activate(inputs_iris[row]) for row in range(len(inputs_iris))]
+#
+# correct = sum(1 for i in range(len(target_iris)) if results[i] == target_iris[i])
+# print(correct/len(target_iris))
 
-print_neuron(and_network)
 
-and_network.train(inputs_and, target_and, 0.001, 10000, 0.5)
+digits = load_digits()
+df_digits = pd.DataFrame(data=np.c_[digits["data"], digits["target"]],
+                     columns= digits["feature_names"] + ["target"])
 
-print("")
-print_neuron(and_network)
-print("------------------------------------------------------------------------")
+digits_network = create_network(2, [10, 5], [64, 10], "digits_network")
+target_digits = []
+for i in range(len(digits["target"])):
+    for j in range(10):
+        if digits["target"][i] == j:
+            target_digits.append(([0]*j+[1]+[0]*(9-j)))
+inputs_digits = [list(row[:64]) for row in df_digits.values]
 
-
-half_adder = create_network(2, [3, 2], [2, 3], "half_adder")
-target_half_adder = [[0,0], [0,1], [0,1], [1,0]]
-inputs_half_adder = list(itertools.product([0, 1], repeat=2))
-
-print_neuron(half_adder)
-
-half_adder.train(inputs_half_adder, target_half_adder, 0.01, 10000, 0.2)
-
-print("")
-print_neuron(half_adder)
-print("------------------------------------------------------------------------")
+digits_network.train(inputs_digits, target_digits, 0.001, 10, 0.5)
 
 
-xor_network = create_network(2, [2, 1], [2, 2], "xor_network")
-inputs_xor = list(itertools.product([0, 1], repeat=2))
-target_xor = [0, 1, 1, 0]
+results_digits = [digits_network.collapse_activate(inputs_digits[row]) for row in range(len(inputs_digits))]
 
-print_neuron(xor_network)
-
-xor_network.train(inputs_xor, target_xor, 0.001, 10000, 0.5)
-
-print("")
-print_neuron(xor_network)
-print("------------------------------------------------------------------------")
-
-
-iris = load_iris()
-df_iris = pd.DataFrame(data=np.c_[iris["data"], iris["target"]],
-                     columns= iris["feature_names"] + ["target"])
-
-iris_network = create_network(2, [4, 3], [4, 4], "iris_network")
-target_iris = []
-for i in range(len(iris["target"])):
-    if iris["target"][i] == 0.0:
-        target_iris.append([1, 0, 0])
-    elif iris["target"][i] == 1:
-        target_iris.append([0, 1, 0])
-    else:
-        target_iris.append([0, 0, 1])
-inputs_iris = [list(row[:4]) for row in df_iris.values]
-
-iris_network.train(inputs_iris, target_iris, 0.001, 1000, 0.5)
-
-results = [iris_network.collapse_activate(inputs_iris[row]) for row in range(len(inputs_iris))]
-
-correct = sum(1 for i in range(len(target_iris)) if results[i] == target_iris[i])
-print(correct/len(target_iris))
+correct_digits = sum(1 for i in range(len(target_digits)) if results_digits[i] == target_digits[i])
+print(correct_digits/len(target_digits))
