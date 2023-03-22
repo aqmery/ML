341763baab98ae266c74f5248f3e8bcc1eb1d389
Henry De Lange <henrydelange8412@gmail.com>
2023-03-22 02:06:02 +0100

    started on reworking the perceptron_learning_rule, finished functions, still needs other changes

--- perceptron_learning_rule/main.py
+++ perceptron_learning_rule/main.py
@@ -37,7 +37,7 @@ def train_perceptron(perceptron, activation, error_threshold=0.05):
         if perceptron.update(p_input, activation, error_threshold):
             print(f"finished training this perceptron in {count} loops")
             stop = True
-        if count == 1000:
+        if count == 10:
             print(f"couldn't train this perceptron in {count} loops")
             stop = True
     print("")
@@ -68,96 +68,105 @@ def train_iris_perceptron(perceptron, p_input, activation, error_threshold=0.05)
 
 
 """creates "and" and "xor" perceptrons and their target values."""
-activation_and = [0, 0, 0, 1]
+p_and_input = list(itertools.product([0, 1], repeat=2))
+p_and_target = [0, 0, 0, 1]
 p_and = Perceptron([random.uniform(-1, 1) for _ in range(2)],
                    random.uniform(-1, 1),
                    "AND")
+# p_and = Perceptron([-0.5, 0.5], 1.5, "AND")
+p_xor_input = list(itertools.product([0, 1], repeat=2))
 activation_xor = [0, 1, 1, 0]
 p_xor = Perceptron([random.uniform(0, 1) for _ in range(2)],
                    random.uniform(-1, 1),
                    "XOR")
 
 
-"""prints the "before training" output of the perceptrons, trains them, and then prints the "after training" output."""
 print_perceptron(p_and)
-train_perceptron(p_and, activation_and)
+
+p_and.train(p_and_input, p_and_target, 0.05, 100, 0.1)
+
 print_perceptron(p_and)
-print("----------------------------------------------------\n")
-print_perceptron(p_xor)
-train_perceptron(p_xor, activation_xor)
-print_perceptron(p_xor)
-print("----------------------------------------------------\n")
+
+# """prints the "before training" output of the perceptrons, trains them, and then prints the "after training" output."""
+# print_perceptron(p_and)
+# train_perceptron(p_and, activation_and)
+# print_perceptron(p_and)
+# print("----------------------------------------------------\n")
+# print_perceptron(p_xor)
+# train_perceptron(p_xor, activation_xor)
+# print_perceptron(p_xor)
+# print("----------------------------------------------------\n")
 
 
-"""loads in the iris dataset, converts it to a pandas DataFrame (df)."""
-iris = load_iris()
-df_iris = pd.DataFrame(data=np.c_[iris["data"], iris["target"]],
-                     columns= iris["feature_names"] + ["target"])
-"""splits the iris df into 2 separate dfs, 1 containing "setosa" and "versicolor", the other containing:
-"setosa" and "virginica".
-creates 2 "iris" perceptrons that have 4 weights and 1 bias.
-as well as well as formatting all the inputs to a list of tuples, and the targets as a list of ints, for each perceptron."""
-df_iris1 = df_iris.loc[df_iris["target"] != 2.0]
-p_iris1 = Perceptron([random.uniform(0, 1) for _ in range(4)],
-                     random.uniform(-1, 1),
-                     "p_iris1")
-iris_input1 = [tuple(row[:4]) for row in df_iris1.values]
-iris_activation1 = [int(row[4]) for row in df_iris1.values]
-
-df_iris2 = df_iris.loc[df_iris["target"] != 1.0]
-p_iris2 = Perceptron([random.uniform(0, 1) for _ in range(4)],
-                     random.uniform(-1, 1),
-                     "p_iris2")
-iris_input2 = [tuple(row[:4]) for row in df_iris2.values]
-iris_activation2 = [int(row[4]) for row in df_iris2.values]
-
-
-"""trains both iris perceptrons in their respected inputs and targets, 
-using a stricter threshold compared to the and and xor perceptrons."""
-train_iris_perceptron(p_iris1, iris_input1, iris_activation1, 0.01)
-train_iris_perceptron(p_iris2, iris_input2, iris_activation2, 0.01)
-print("----------------------------------------------------\n")
-
-
-"""adds the results and the amount of correct classifications to 2 lists,
- and calculates the percentage of correct classifications.
- also prints the name, final bias and final weights of the perceptrons."""
-results1 = [p_iris1.activate(row[:4]) for row in df_iris1.values]
-correct1 = sum(1 for i in range(len(iris_activation1)) if results1[i] == iris_activation1[i])
-print(p_iris1)
-print(f"percentage correct = {correct1/len(iris_activation1)*100}%")
-print("\n----------------------------------------------------\n")
-
-
-"""because the target for the "virginica" is 2, and the perceptrons currently use the step_function, 
-it's impossible to get a 2 as the target output.
-there are a few ways to "fix" this, one of them being, changing the step_function output from 1 to 2.
-because for this assignment I only want to show the accuracy of the training model,
-I chose to multiply the result by 2, this changes a 1 into a 2, but keeps a 0 as a 0."""
-results2 = [p_iris2.activate(row[:4])*2 for row in df_iris2.values]
-correct2 = sum(1 for i in range(len(iris_activation2)) if results2[i] == iris_activation2[i])
-print(p_iris2)
-print(f"percentage correct = {correct2/len(iris_activation2)*100}%")
-
-
-"""in conclusion:
-3.a. the final parameters of the and perceptron are:
-    weights = [2.099383380832772, 0.8017412383211584], bias = -2.124479997506068
-
-3.b. the final parameters of the xor perceptron are:
-    weights = [4.494618387453568, 4.13285886300068], bias = -1.834556204282468
-    but obviously it doesn't work, as it needs more than 1 layer to function.
-
-3.c.i. the final parameters of the iris1 perceptron are:
-    weights = [7.622027044609713, 7.2885968113681345, 7.58056793096198, 7.4579294510930305], bias = -85.0322175923695
-
-3.c.ii. the final parameters of the iris2 perceptron are:
-    weights = [3.2551122422360015, 3.662541654494767, 3.3763164577640734, 4.004124690069453], bias = -41.70539527849178
-    
-
-interestingly the iris2 perceptron is really quick to learn, even with a higher error threshold,
-getting a 100% accuracy on the "setosa" and "virginica" data in only a few runs and with a lot more certainty.
-looking over this data it's quite clear why, as there is a large difference in sizes for multiple parameters,
-the difference between the "setosa" and "versicolor" data isn't as strong, 
-therefore it takes the iris1 perceptron a lot longer to train, and doesn't score a 100% accuracy, which is to be expected.
-"""
+# """loads in the iris dataset, converts it to a pandas DataFrame (df)."""
+# iris = load_iris()
+# df_iris = pd.DataFrame(data=np.c_[iris["data"], iris["target"]],
+#                      columns= iris["feature_names"] + ["target"])
+# """splits the iris df into 2 separate dfs, 1 containing "setosa" and "versicolor", the other containing:
+# "setosa" and "virginica".
+# creates 2 "iris" perceptrons that have 4 weights and 1 bias.
+# as well as well as formatting all the inputs to a list of tuples, and the targets as a list of ints, for each perceptron."""
+# df_iris1 = df_iris.loc[df_iris["target"] != 2.0]
+# p_iris1 = Perceptron([random.uniform(0, 1) for _ in range(4)],
+#                      random.uniform(-1, 1),
+#                      "p_iris1")
+# iris_input1 = [tuple(row[:4]) for row in df_iris1.values]
+# iris_activation1 = [int(row[4]) for row in df_iris1.values]
+#
+# df_iris2 = df_iris.loc[df_iris["target"] != 1.0]
+# p_iris2 = Perceptron([random.uniform(0, 1) for _ in range(4)],
+#                      random.uniform(-1, 1),
+#                      "p_iris2")
+# iris_input2 = [tuple(row[:4]) for row in df_iris2.values]
+# iris_activation2 = [int(row[4]) for row in df_iris2.values]
+#
+#
+# """trains both iris perceptrons in their respected inputs and targets,
+# using a stricter threshold compared to the and and xor perceptrons."""
+# train_iris_perceptron(p_iris1, iris_input1, iris_activation1, 0.01)
+# train_iris_perceptron(p_iris2, iris_input2, iris_activation2, 0.01)
+# print("----------------------------------------------------\n")
+#
+#
+# """adds the results and the amount of correct classifications to 2 lists,
+#  and calculates the percentage of correct classifications.
+#  also prints the name, final bias and final weights of the perceptrons."""
+# results1 = [p_iris1.activate(row[:4]) for row in df_iris1.values]
+# correct1 = sum(1 for i in range(len(iris_activation1)) if results1[i] == iris_activation1[i])
+# print(p_iris1)
+# print(f"percentage correct = {correct1/len(iris_activation1)*100}%")
+# print("\n----------------------------------------------------\n")
+#
+#
+# """because the target for the "virginica" is 2, and the perceptrons currently use the step_function,
+# it's impossible to get a 2 as the target output.
+# there are a few ways to "fix" this, one of them being, changing the step_function output from 1 to 2.
+# because for this assignment I only want to show the accuracy of the training model,
+# I chose to multiply the result by 2, this changes a 1 into a 2, but keeps a 0 as a 0."""
+# results2 = [p_iris2.activate(row[:4])*2 for row in df_iris2.values]
+# correct2 = sum(1 for i in range(len(iris_activation2)) if results2[i] == iris_activation2[i])
+# print(p_iris2)
+# print(f"percentage correct = {correct2/len(iris_activation2)*100}%")
+#
+#
+# """in conclusion:
+# 3.a. the final parameters of the and perceptron are:
+#     weights = [2.099383380832772, 0.8017412383211584], bias = -2.124479997506068
+#
+# 3.b. the final parameters of the xor perceptron are:
+#     weights = [4.494618387453568, 4.13285886300068], bias = -1.834556204282468
+#     but obviously it doesn't work, as it needs more than 1 layer to function.
+#
+# 3.c.i. the final parameters of the iris1 perceptron are:
+#     weights = [7.622027044609713, 7.2885968113681345, 7.58056793096198, 7.4579294510930305], bias = -85.0322175923695
+#
+# 3.c.ii. the final parameters of the iris2 perceptron are:
+#     weights = [3.2551122422360015, 3.662541654494767, 3.3763164577640734, 4.004124690069453], bias = -41.70539527849178
+#
+#
+# interestingly the iris2 perceptron is really quick to learn, even with a higher error threshold,
+# getting a 100% accuracy on the "setosa" and "virginica" data in only a few runs and with a lot more certainty.
+# looking over this data it's quite clear why, as there is a large difference in sizes for multiple parameters,
+# the difference between the "setosa" and "versicolor" data isn't as strong,
+# therefore it takes the iris1 perceptron a lot longer to train, and doesn't score a 100% accuracy, which is to be expected.
+# """
--- perceptron_learning_rule/perceptron.py
+++ perceptron_learning_rule/perceptron.py
@@ -38,48 +38,109 @@ class Perceptron:
         weighted_sum = weighted_sum + self.bias
         return self.step_function(weighted_sum)
 
-    def update(self, p_input, activation, error_threshold):
-        """
-        calculates how "wrong" a prediction is, appends this to the "weight_error" list.
-        using this list, calculates the MSE calling the "loss" function,
-        as well as updating the weights of the perceptron calling the "update_weights" function.
-        :param p_input: gets a list of tuples with all available inputs.
-        :param activation: gets a list of ints with the target value for each tuple of the previous input.
-        :param error_threshold: gives the "max" value for the loss function, if the loss function is lower, end the loop.
-        :return: True when the error threshold is achieved, returns False if not.
-        """
-        weight_error = []
-        for i_input in range(len(p_input)):
-            temp_error = round(expit(sum([self.weights[i] * p_input[i_input][i] for i in range(len(self.weights))]) + self.bias), 3)
-            weight_error.append(temp_error)
-        if self.loss(weight_error, activation) < error_threshold:
-            return True
-        self.update_weights(weight_error, activation)
-        return False
+    def calculate_error(self, activation, target):
+        error = target-activation
+        return error
 
-    def update_weights(self, weight_error, activation):
-        """
-        calculates the error of the bias and each weight, and changes it based on the error.
-        :param weight_error: gets a list of floats that indicate the error of each weight, compared to the target.
-        :param activation: gets the list of targets.
-        :return: changes the self.weights and self.bias instance variables, doesn't return anything.
-        """
-        for i_result in range(len(activation)):
-            error = round(weight_error[i_result]*((1-weight_error[i_result])*
-                                                  -(activation[i_result]-weight_error[i_result])), 3)
-            for i_weight in range(len(self.weights)):
-                self.weights[i_weight] = self.weights[i_weight] - self.eta * activation[i_result] * error
-            self.bias = self.bias - self.eta * 1 * error
+    def calculate_weight_delta(self, error, inputs):
+        weight_changes = []
+        for j in range(len(self.weights)):
+            weight_changes.append((self.eta*error*inputs[j]))
+        bias_change = self.eta*error
+        return weight_changes, bias_change
 
-    def loss(self, weight_error, activation):
-        """
-        calculates the mean squared error (MSE).
-        :param weight_error: gets a list of floats that indicate the error of each weight, compared to the target.
-        :param activation: gets the list of targets.
-        :return: calculates the MSE and returns this as a float.
-        """
-        error_sum = 0
-        for i in range(len(weight_error)):
-            error_sum += (activation[i]-weight_error[i])**2
-        mean_squared_error = error_sum/(2*(len(weight_error)))
-        return mean_squared_error
+    def update(self, weight_changes, bias_change):
+        for i in range(len(self.weights)):
+            self.weights[i] = self.weights[i]+weight_changes[i]
+        self.bias = self.bias+bias_change
+
+    def loss(self, inputs, target):
+        errors = []
+        for i in range(len(target)):
+            errors.append((self.calculate_error(self.activate(inputs[i]), target[i]))**2/2)
+        print(errors)
+        mse = (sum(errors))/(len(target))
+        return mse
+
+    def train(self, inputs, target, error_threshold, train_threshold, eta):
+        self.eta = eta
+        cont = True
+        epochs = 0
+        while cont:
+            # out = []
+            # print(self.weights)
+            # print(self.bias)
+            for i in range(len(target)):
+                # out.append(self.activate(inputs[i]))
+                error = self.calculate_error(self.activate(inputs[i]), target[i])
+                # print(error)
+                weight_changes, bias_change = self.calculate_weight_delta(error, inputs[i])
+                self.update(weight_changes, bias_change)
+            epochs += 1
+            # print(out)
+            loss = self.loss(inputs, target)
+            print(loss)
+            if loss <= error_threshold:
+                print("epochs trained :", epochs)
+                cont = False
+            if epochs >= train_threshold:
+                print("epochs trained :", epochs)
+                cont = False
+            # cont = False
+
+
+
+
+
+
+
+
+
+
+
+    # def update(self, p_input, activation, error_threshold):
+    #     """
+    #     calculates how "wrong" a prediction is, appends this to the "weight_error" list.
+    #     using this list, calculates the MSE calling the "loss" function,
+    #     as well as updating the weights of the perceptron calling the "update_weights" function.
+    #     :param p_input: gets a list of tuples with all available inputs.
+    #     :param activation: gets a list of ints with the target value for each tuple of the previous input.
+    #     :param error_threshold: gives the "max" value for the loss function, if the loss function is lower, end the loop.
+    #     :return: True when the error threshold is achieved, returns False if not.
+    #     """
+    #     weight_error = []
+    #     for i_input in range(len(p_input)):
+    #         temp_error = round(expit(sum([self.weights[i] * p_input[i_input][i] for i in range(len(self.weights))]) + self.bias), 3)
+    #         print(temp_error)
+    #         weight_error.append(temp_error)
+    #     if self.loss(weight_error, activation) < error_threshold:
+    #         return True
+    #     self.update_weights(weight_error, activation)
+    #     return False
+    #
+    # def update_weights(self, weight_error, activation):
+    #     """
+    #     calculates the error of the bias and each weight, and changes it based on the error.
+    #     :param weight_error: gets a list of floats that indicate the error of each weight, compared to the target.
+    #     :param activation: gets the list of targets.
+    #     :return: changes the self.weights and self.bias instance variables, doesn't return anything.
+    #     """
+    #     for i_result in range(len(activation)):
+    #         error = round(weight_error[i_result]*((1-weight_error[i_result])*
+    #                                               -(activation[i_result]-weight_error[i_result])), 3)
+    #         for i_weight in range(len(self.weights)):
+    #             self.weights[i_weight] = self.weights[i_weight] - self.eta * activation[i_result] * error
+    #         self.bias = self.bias - self.eta * 1 * error
+
+    # def calculate_loss(self, weight_error, activation):
+    #     """
+    #     calculates the mean squared error (MSE).
+    #     :param weight_error: gets a list of floats that indicate the error of each weight, compared to the target.
+    #     :param activation: gets the list of targets.
+    #     :return: calculates the MSE and returns this as a float.
+    #     """
+    #     error_sum = 0
+    #     for i in range(len(weight_error)):
+    #         error_sum += (activation[i]-weight_error[i])**2
+    #     mean_squared_error = error_sum/(2*(len(weight_error)))
+    #     return mean_squared_error
